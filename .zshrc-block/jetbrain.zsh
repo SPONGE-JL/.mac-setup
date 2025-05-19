5c577ba9186b3014646fa2ecc6b2b168f9fe549a
SPONGE-JL <dev2sponge@gmail.com>
2025-05-19 21:02:41 +0900

    feat: support Jetbrain Toolbox CLI - idea, pycharm, etc

---
+++ .zshrc-block/jetbrain.zsh
@@ -0,0 +1,3 @@
+# ---
+# Jetbrain Toolbox CLI - https://www.jetbrains.com/help/idea/working-with-the-ide-features-from-command-line.html#macos-scripts
+export PATH="${HOME}/Library/Application Support/JetBrains/Toolbox/scripts:${PATH}"
