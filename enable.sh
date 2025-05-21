047f83dc7b35408ddd9122b562dd1ba00688f71b
SPONGE-JL <dev2sponge@gmail.com>
2025-05-21 21:25:22 +0900

    fix: ignore .vscode/ folder to avoid crash

--- enable.sh
+++ enable.sh
@@ -1,10 +1,15 @@
 #!/bin/zsh
 stow --ignore=.DS_Store \
+     --ignore=.vscode \
      --ignore=.commitlintrc.yaml \
      --ignore=.editorconfig \
      --ignore=.gitignore \
+     --ignore=.go-version \
      --ignore=.markdownlint.yaml \
+     --ignore=.nvmrc \
      --ignore=.pre-commit-config.yaml \
+     --ignore=.python-version \
+     --ignore=.sdkmanrc \
      --ignore=.yarmlint.yaml \
      --ignore=guide \
      --ignore=disable.sh \
