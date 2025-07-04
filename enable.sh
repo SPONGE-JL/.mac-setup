4247daf3593d01b1bbc07293871587c63ecef6b3
sponge <48089867+SPONGE-JL@users.noreply.github.com>
2025-07-04 21:17:20 +0900

    feat(script): auto refresh end of enable.sh

--- enable.sh
+++ enable.sh
@@ -1,4 +1,18 @@
 #!/bin/zsh
+
+echo " "
+echo "❯ Enable symlinks"
+echo " "
+
+# Pull latest case
+GIT_OPT="--quiet"
+git fetch --all --tags --prune --prune-tags --atomic --auto-maintenance ${GIT_OPT}
+git pull --autostash --rebase --show-forced-updates --summary ${GIT_OPT} origin $(git branch --show-current)
+
+# Prune previous symlinks
+stow --delete .
+
+# Create symlinks
 stow --ignore=.DS_Store \
      --ignore=.aws \
      --ignore=.vscode \
@@ -18,3 +32,9 @@ stow --ignore=.DS_Store \
      .
 
 ls -shalt ${HOME} | grep ' l' || echo 'empty'
+
+echo " "
+echo "❯ Refresh your shell prompt"
+echo " "
+
+exec ${SHELL:-/usr/bin/zsh}
