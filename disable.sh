dd8c5c494f75168bba7a0c54cf856e49d0426069
sponge <48089867+SPONGE-JL@users.noreply.github.com>
2025-07-04 21:18:03 +0900

    chore(script): add guide message for disable.sh

--- disable.sh
+++ disable.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+
+echo " "
+echo "❯ Disable symlinks"
+echo " "
+
+# Remove
 stow --delete .
 
-ls -shalt ${HOME} | grep ' l' || echo 'empty'
+ls -shalt ${HOME} | grep " l" || echo "empty"
+echo " "
